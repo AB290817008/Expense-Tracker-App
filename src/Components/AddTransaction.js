9b279dd23d7ed7982a3295f2ea7bb1625559f079
AB290817008 <42501497+AB290817008@users.noreply.github.com>
2020-06-18 18:13:01 +0500

    ExpenseTrackerApp

---
+++ src/Components/AddTransaction.js
@@ -0,0 +1,59 @@
+import React,{useState,useContext} from 'react'
+
+
+
+import {GlobalContext} from '../Context/GlobalState';
+
+
+
+export const AddTransaction = () => {
+   
+    const {addTransaction} = useContext(GlobalContext);
+
+
+    const onSubmit =e => {
+        e.preventDefault()}
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+        addTransaction(newTransaction);
+    }
+
+
+    const [text,setText] = useState('')
+
+    const [amount,setAmount] = useState(0)
+
+   
+   
+   
+    return (
+        
+        
+
+
+        <div>
+            <h3>Add new transaction</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-control">
+                    <label htmlFor="text">Details</label>
+                    <input type="text" id="text" placeholder="Enter text..." value={text} onChange={(e) => setText(e.target.value)}/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">
+                    Amount <br />
+                    (negative - expense, positive + income)</label>
+                
+                    <input type="number" id="amount" placeholder="Enter amount..." value={amount} onChange={(e) => setAmount(e.target.value)}/>
+                </div>
+                <button className="btn">Add transaction</button>
+            </form>
+        </div>
+    )
+
+
+    
+
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h1>Xpense Tracker App by AB</h1>    
+        </div>
+    )
+}
