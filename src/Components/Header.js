9dab94f5f227b44bbedfbf8e45ffaf7c549f83bf
AB290817008 <42501497+AB290817008@users.noreply.github.com>
2020-12-13 18:17:47 +0500

    Updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            <h1>X-pense(Expense) Tracker App by <span className="name">Arsal Abbasi</span></h1>    
+            {/* <span className="name">Arsal Abbasi</span> */}
+            <h1>Simple Expense Tracker App </h1>    
         </div>
     )
 }
+
